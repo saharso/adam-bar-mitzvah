db92904b0a74679440dab922439d73ffe9256e0e
saharsoffer <saharso@gmail.com>
2022-04-10 17:15:33 +0300

    add slides

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,59 @@
-import React, {PropsWithChildren, useCallback, useRef, useState} from 'react';
+import React, {PropsWithChildren, ReactNode, useCallback, useMemo, useRef, useState} from 'react';
 import './App.scss';
 import Slider from "react-slick";
 import Welcome from "./slides/Welcome/Welcome";
 import BackGroundMasonry from "./parts/BackGroundMasonry/BackGroundMasonry";
-import {Slide1Birth, Slide2Birth} from "./slides/Slide1Birth/Slide1Birth";
-declare module 'react-slick'
+import {Slide1Birth, Slide2Birth, Slide3Birth, Slide4Birth} from "./slides/Slide1Birth/Slide1Birth";
+declare module 'react-slick';
+type Categories = "welcome" | "early_childhood";
+const categories: Record<Categories, string> = {
+    welcome: "",
+    early_childhood:"ההתחלה",
+}
+interface Slide {
+    category: Categories;
+    component: ReactNode;
+}
+const slides: Slide[] = [
+    {
+        category: "welcome",
+        component: <Welcome/>,
+    },
+    {
+        category: "early_childhood",
+        component: <Slide1Birth/>,
+    },
+    {
+        category: "early_childhood",
+        component: <Slide2Birth/>,
+    },
+    {
+        category: "early_childhood",
+        component: <Slide3Birth/>,
+    },
+    {
+        category: "early_childhood",
+        component: <Slide4Birth/>,
+    },
+]
+function* make(direction: [0|1|-1]){
+    let counter = 0;
+    while(true){
+        counter += direction[0];
+        if(counter < 0){
+            counter = 0;
+        } else if (counter >= slides.length){
+            counter = slides.length -1;
+        }
+        yield slides[counter];
+    }
+}
 
 function App() {
-
     const sliderRef = useRef<any>();
+    const direction = useRef<[0|1|-1]>([0]);
+    const [category, setCategory] = useState<Categories>();
+    const gen = useMemo(()=>make(direction.current), [])
     const settings = {
         infinite: false,
         speed: 500,
@@ -18,26 +63,26 @@ function App() {
     };
 
     const handleNext = useCallback(()=>{
+        direction.current[0] = 1;
+        setCategory(gen.next().value?.category);
         sliderRef.current.slickNext();
     },[]);
     const handlePrev = useCallback(()=>{
+        direction.current[0] = -1;
+        setCategory(gen.next().value?.category);
         sliderRef.current.slickPrev();
     },[]);
     return (
     <div className="App">
         <MainPanel>
-            <CategoryLabel/>
+            <CategoryLabel category={category || "welcome"}/>
             {/* @ts-ignore */}
             <Slider {...settings} className={'slider'} ref={sliderRef}>
-                <Slide>
-                    <Welcome/>
-                </Slide>
-                <Slide>
-                    <Slide1Birth/>
-                </Slide>
-                <Slide>
-                    <Slide2Birth/>
-                </Slide>
+                {slides.map((item, index)=>{
+                    return <Slide key={index}>
+                        {item.component}
+                    </Slide>
+                })}
             </Slider>
             <footer>
                 <button onClick={handleNext}>next</button>
@@ -49,8 +94,9 @@ function App() {
   );
 }
 
-export function CategoryLabel(){
-    return <div>TEST</div>
+export function CategoryLabel(props: {category: Categories}){
+    const {category} = props;
+    return <div>{categories[category]}</div>
 }
 
 export function MainPanel({children}: PropsWithChildren<unknown>){
